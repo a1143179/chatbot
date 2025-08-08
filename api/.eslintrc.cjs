8c6429b6af64867aafee129ac5b89bb73a9acd0a
ice <a1143179@gmail.com>
2025-08-08 14:15:58 +1000

    Add explicit Jest globals to ESLint override for test files

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -7,6 +7,16 @@ module.exports = {
       files: ["**/__tests__/**/*.js", "**/*.test.js"],
       env: {
         jest: true
+      },
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        jest: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly'
       }
     }
   ]
