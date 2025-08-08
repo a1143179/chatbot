9700f91ad2d62ce301253960e3ec3a2e07cf4d30
ice <a1143179@gmail.com>
2025-08-08 14:14:05 +1000

    Add Jest globals to ESLint config to fix test lint errors

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -2,5 +2,11 @@ module.exports = {
   env: {
     node: true,
     jest: true
+  },
+  globals: {
+    describe: 'readonly',
+    it: 'readonly',
+    expect: 'readonly',
+    jest: 'readonly'
   }
 };
