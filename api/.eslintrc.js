2ff7f92d1a6307d919b2ea33004619be2bb3cd3f
ice <a1143179@gmail.com>
2025-08-08 14:28:13 +1000

    Switch to .eslintrc.js, add jest env in overrides for full Jest lint support

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,6 +1,15 @@
 module.exports = {
   env: {
-    node: true,
-    jest: true
-  }
+    node: true
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.js", "**/*.test.js"],
+      env: {
+        jest: true
+      },
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended']
+    }
+  ]
 };
