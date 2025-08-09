751f32fb6cb202709bf4462a4e3d0d4f8c845b18
ice <a1143179@gmail.com>
2025-08-09 15:29:00 +1000

    automated commit

--- api/knexfile.cjs
+++ api/knexfile.cjs
@@ -1,3 +1,6 @@
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_HOST:", process.env.DB_HOST);
 module.exports = {
   development: {
     client: 'mssql',
